e8eed09892b172999e26b1e596bd889027a52202
pingshen670822 <pingshen670822@users.noreply.github.com>
2026-07-16 12:14:20 +0800

    Publish mobile 539 safe cloud version 20260716121407

--- site/service-worker.js
+++ site/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="539-mobile-20260716091311";
+const CACHE="539-mobile-20260716121407";
 async function clearAllCaches(){
   const keys=await caches.keys();
   await Promise.all(keys.map(key=>caches.delete(key)));
